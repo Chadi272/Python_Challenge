78c8b7ff2147c95d7a92335bd9c75ddb80ee8e6c
ChadiGhosn <chadighosn272@gmail.com>
2022-06-19 14:44:11 +0100

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,14 +1,74 @@
+#Importing modules
 import os
 import csv
-
-#file = open('C:\Users\chadi\Documents\GitHub\Python_Challenge\PyBank\Resources\budget_data.csv')
-#csvfile = csv.reader(file)
-
+#Path to Excel csv file
 csvpath = os.path.join(os.path.expanduser('~'),'Documents','GitHub', 'Python_Challenge','PyBank','Resources','budget_data.csv')
-
+textpath = os.path.join(os.path.expanduser('~'),'Documents','GitHub', 'Python_Challenge','PyBank','Analysis','Analysis.txt')
+#create text file
+f = open(textpath, "w")
+#Read file to work with data
 with open(csvpath) as csvfile:
-
-    # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
+    #Start variables
+    totalv = 0
+    rowcount = 0
+    maxm = 0
+    #Define list
+    Mylist = []
+    Mynlist = []
+    Changepermonthlist = []
+    #Skip headers
+    next(csvreader)
+    #Starting the loop through the rows
+    for row in csvreader:
+        #Total months
+        rowcount += 1
+        #Total P/L
+        totalv += int(row[1])
+        #Set the column values as integer
+        rowv = int(row[1])
+        #Define lists
+        Mynlist += [rowv]
+        Mynlist.pop(0)
+        #Change in Profit/Loss
+        zip_object = zip(Mylist, Mynlist)
+        changelist = []
+        for Mylist, Mynlist in zip_object:
+            changelist.append(Mynlist-Mylist)
+        change = sum(changelist)
+        #Add values in Profit/Loss to the list
+        Mylist += [rowv]
+        #Max value in the list
+        maxv = max(Mylist)
+        #Min value in the list
+        minv = min(Mylist)
+        #Find month for max value
+        if maxv == int(row[1]):
+            maxm = row[0]
+        #Find month for min value
+        if minv == int(row[1]):
+            minm = row[0]
+        #Format values
+        maxvl = '{:,}'.format(maxv)
+        minvl = '{:,}'.format(minv)
 
-    print(csvreader)
+    print("Financial Analysis")
+    print("---------------------------")
+    print(f'Total Months: {int(rowcount)}')
+    print(f'Total: ${"{:,}".format(totalv)}')
+    print(f'Greatest Increase in Profits: {maxm} (${maxvl})')
+    print(f'Greatest Decrease in Profits: {minm} (${minvl})')
+    print(change)
+
+f.write("Financial Analysis")
+f.write('\n')
+f.write("---------------------------")
+f.write('\n')
+f.write(f'Total Months: {int(rowcount)}')
+f.write('\n')
+f.write(f'Total: ${"{:,}".format(totalv)}')
+f.write('\n')
+f.write(f'Greatest Increase in Profits: {maxm} (${maxvl})')
+f.write('\n')
+f.write(f'Greatest Decrease in Profits: {minm} (${minvl})')
+
